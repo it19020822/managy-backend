6e76ec919b26cc9822f636027540b78b0a37619f
it19020822 <it19020822@my.sliit.lk>
2022-10-31 21:51:36 +0530

    database connection established

--- models/userModel.js
+++ models/userModel.js
@@ -1,16 +1,33 @@
-const mongoose  = require('mongoose');
+const mongoose = require('mongoose');
 
-const Schema  = mongoose.Schema;
+const Schema = mongoose.Schema;
 
-const userSchema  = new Schema ({
-        name : {type : String, required : true},
-        email : {type : String, required : true},
-        password : {type : String, required : true},
-        gender : {type : String, required : true},
-        type : {type : String , required :true},
-        phoneNumber : {type : Number, required : true}
-},{
-    timestamps:true
+const userSchema = new Schema({
+    name: { 
+        type: String, 
+        required: true 
+    },
+    email: { 
+        type: String, 
+        required: true 
+    },
+    password: { 
+        type: String
+    },
+    emailToken: {
+        type: String
+    },
+    gender: { 
+        type: String
+    },
+    isVerified: {
+        type: Boolean
+    },
+    type: { 
+        type: String
+    }
+}, {
+    timestamps: true
 });
 
 const User = mongoose.model('User', userSchema);
